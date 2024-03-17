6f9717e7694e60043915aad01c013130638f37f0
Habiba <160347457+Habiba-Wasim@users.noreply.github.com>
2024-03-17 23:10:11 +0500

    Step 44 of Typescript
    Step 44 of Typescript

---
+++ Q44.ts
@@ -0,0 +1,15 @@
+// Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+
+function Sandwich(...items: string[]): void {
+    console.log("Sandwich order:");
+    for (let i = 0; i < items.length; i++){
+        console.log(`- ${items[i]}`);
+        
+    }
+}
+console.log("Enjoy your Sandwish Maam Nida");
+
+Sandwich('Capsicum', 'Chicken', 'Garlic Ketchup')
+Sandwich('Cheese', 'Beef')
+Sandwich('MAYO Sause', 'BBQ Chicken')
+
